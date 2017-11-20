65897e494a3c7baf1deb28b5d55fef517096d7b2
Eoghan-oconnor <G00330972@gmit.ie>
2017-11-20 19:06:43 +0000

    Adding .gitignore and tiding up

--- eliza.go
+++ eliza.go
@@ -4,3 +4,24 @@
 	3rd year software project 
 */
 
+
+package main
+
+import(
+
+	"net/http"
+
+)
+
+//serving the webpage
+func requestHandler(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w,r, "index.html")
+
+}
+
+func main(){
+
+	http.HandleFunc("/",requestHandler)
+
+	http.ListenAndServe(":8080",nil)
+}
