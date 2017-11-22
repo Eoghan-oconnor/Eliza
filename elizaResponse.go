d88f7ed184c9a206f7b640ad633abe1bd14a9f86
Eoghan-oconnor <G00330972@gmit.ie>
2017-11-22 23:43:54 +0000

    Adding more to elizaResponse

--- elizaResponse.go
+++ elizaResponse.go
@@ -6,4 +6,37 @@ var phrases = map[string][]string{
     `hello`: {
         "Hello How are you feeling?",   
     },
+}  
+
+var convoRestart= []string{
+    "I'm sorry but i dont understand what you're saying",
+    "Let's focus on whats important.... how are you feeling",
+	"Tell me more.",
+}
+
+//changes the pronoun to allow conversations to make sense
+var Reflections = map[string]string{
+    "am":       "are",
+    "was":      "were",
+    "I":        "you",
+    "I'd":      "you would",
+    "I've":     "you have",
+    "I'll":     "you will",
+    "my":       "your",
+    "are":      "am",
+    "you've":   "I have",
+    "you'll":   "I will",
+    "you're":   "I'm",
+    "your":     "my",
+    "yours":    "mine",
+    "you":      "I",
+    "me":       "you",
+}
+
+//Allows user to enter words similar and have it still make sense
+var Synonymizer = map[string][]string{
+    // Expressions
+   
+    "hello":    []string{"hey","hi","greetings","howya"},
     
+}
